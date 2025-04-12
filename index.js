452d4de02ff5e7fa3ed2d744ce158803227f517a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-04-12 12:26:11 +0530

    project started

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const app = require("./app");
+const { PORT = 5000 } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to MongoDB with Prisma");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+};
+
+connectDB();
+
+module.exports = prisma;
